6a2a68c187a42d6f77f083542fde6dfa64c9d485
YGainey <yousef.gainey@gmail.com>
2024-08-30 11:08:31 +0100

    fixed some settings

--- aipplicant/settings.py
+++ aipplicant/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -81,6 +80,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://aipplicant-8e0132de884b.herokuapp.com"
 ]
 
 ROOT_URLCONF = 'aipplicant.urls'

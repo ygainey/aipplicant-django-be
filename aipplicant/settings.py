ffeb2abf761d51c7fb8cd7c8d6cd22bffa3e8cd5
YGainey <yousef.gainey@gmail.com>
2024-08-30 12:41:48 +0100

    blah

--- aipplicant/settings.py
+++ aipplicant/settings.py
@@ -89,7 +89,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://aipplicant-8e0132de884b.herokuapp.com",
-    "https://main--aipplicant.netlify.app/"
+    "https://main--aipplicant.netlify.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True

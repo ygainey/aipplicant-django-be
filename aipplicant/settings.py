a45ffbbb0a46f7a30894ef9a41b0c6729e43eaa3
YGainey <yousef.gainey@gmail.com>
2024-08-30 11:28:31 +0100

    blah

--- aipplicant/settings.py
+++ aipplicant/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "https://aipplicant-8e0132de884b.herokuapp.com",

5af0a2f3d5ea6c5e31cac4550a369c9e943efaee
YGainey <yousef.gainey@gmail.com>
2024-09-28 03:01:48 +0100

    cors broke trying to fix

--- aipplicant/settings.py
+++ aipplicant/settings.py
@@ -89,7 +89,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://aipplicant-8e0132de884b.herokuapp.com",
-    "https://aipplicant.netlify.app/"
+    "https://aipplicant.netlify.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
